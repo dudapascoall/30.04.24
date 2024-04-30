e37f520b388380368a7f42307b31bd6885b8324d
dudapascoall <142435751+dudapascoall@users.noreply.github.com>
2024-04-30 11:25:38 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,24 @@
+class Objeto{
+constructor (parametro1, parametro2) {
+     // quando nao tem é abstrato
+        this.parametro1 = parametro1 //variavel de interfase 
+        this.parametro2 = parametro2
+    }
+
+    metodo1 () {
+        //açoes
+    }
+
+    metodo1 () {
+
+    }
+}
+class Calebe {
+    constructor (tiktok) {
+        this.site = tiktok;
+    }
+
+    exibirSite () {
+        window.open(this.site)
+    }
+}
